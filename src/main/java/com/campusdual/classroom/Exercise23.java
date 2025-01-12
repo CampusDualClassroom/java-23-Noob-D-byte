0a44d7091eb1370c9c0794735d8e1a6cb3911117
davidrey <david_sardy@hotmail.com>
2025-01-12 17:55:03 +0100

    Ejercicio 23 Resolto

--- src/main/java/com/campusdual/classroom/Exercise23.java
+++ src/main/java/com/campusdual/classroom/Exercise23.java
@@ -5,16 +5,43 @@ import java.util.Map;
 
 public class Exercise23 {
 
-    public static Map<String, Person> createHashMap() {
+    public static void main(String[] args) {
+        Map<String, Person> map = createHashMap();
+        Person newPolice = new PoliceOfficer("Charles", "Boyle", "B-99");
+        addMapValue(map, "police", newPolice);
+        printMapValues(map);
+    }
 
+    public static Map<String, Person> createHashMap() {
+        //Crear una colección de tipo Map e inicializarlo como HashMap en el metodo createHashMap().
+
+        Map<String, Person> map = new HashMap<>();
+
+        // Luego poblar el mapa con diferentes entradas
+        map.put("person", new Person("John", "Smith"));
+        map.put("teacher", new Teacher("María", "Montessori", "Educación"));
+        map.put("police", new PoliceOfficer("Jake", "Peralta", "B-99"));
+        map.put("doctor", new Doctor("Gregory", "House", "Nefrología e infectología"));
+        //y devolver el mapa creado.
+
+        return map;
     }
 
     public static Person addMapValue(Map<String, Person> map, String key, Person value) {
+        //Añadir una entrada más al mapa en el metodo addMapValue()
+        map.put(key, value);
+
+        return value;// y devolver el objeto que se añadió como value de la entry.
 
     }
 
     public static void printMapValues(Map<String, Person> map) {
+        for (Map.Entry<String, Person> entry : map.entrySet()) {
+            System.out.println("Clave " + entry.getKey() + ", ");
+            entry.getValue().getDetails();//Vai fora do System.out.printl porque estes métodos xa teñen integrado un sout
+        }
+
 
     }
-    
+
 }
